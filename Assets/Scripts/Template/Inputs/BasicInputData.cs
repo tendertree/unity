abda07e7f80063e562ebe11cf345a531517a750d
tendertree <tendertree@gmail.com>
2024-07-27 17:40:55 +0900

    feat: reboot

--- Assets/Scripts/Template/Inputs/BasicInputData.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-public struct BasicInputData : IComponentData
-{
-  public float Click; 
-  public float2 Direction;
-  public float2 MousePostion;
-}
